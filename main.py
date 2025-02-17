c178aa6ec8698b2186d2961b6793223a434cf835
Achilleas Makris <lexlarisa@hotmail.com>
2025-02-17 21:30:34 +0200

    προσθήκη του Delete function

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ def task_add(tasks, total_hours):
         try:    
             name = input("Δώσε το όνομα του στόχου: ").strip()
 
-            # Validate name: Must contain only letters and spaces
+            # Έλεγχος του ονόματος και αφαίρεση κενών για να γίνει 1 λέξη
             if not name.replace(" ", "").isalpha():
                 raise ValueError("Λάθος όνομα του Task, παρακαλώ γράψτε μια λέξη.")
             
             hours = float(input("Δώσε την εβδομαδιαία διάρκεια σε ώρες: "))
-
+            # Έλεγχος αν ο χρήστης βάλει παραπάνω απο τις διαθέσιμες ώρες της εβδομάδας
             if not (0 <= hours <= 168):
                 raise ValueError("Λάθος αριθμός χρόνου του στόχου.")
             if total_hours + hours > 168:
@@ -34,6 +34,7 @@ def task_add(tasks, total_hours):
 
             importance = int(input("Δώσε τον βαθμό σημαντικότητας του στόχου: "))
 
+            #Έλεγχος αν ο χρήστης έβαλε σωστή τιμή
             if not (1 <= importance <= 10):
                 raise ValueError("Ο αριθμός πρέπει να είναι απο 1 - 10")
 
@@ -42,6 +43,9 @@ def task_add(tasks, total_hours):
             continue
 
         free_time = float(input("Δώσε τον ελεύθερο χρόνο που έχεις συνολικά για την εβδομάδα: "))
+
+        # Έλεγχος του διαθέσιμου χρόνου της εβδομάδας
+        #TODO Να το κάνω Global και όχι σε κάθε νέο τασκ να το ξανά ζητάει!
         if not 1 <= free_time <= 168:
             raise ValueError("Δεν μπορείς να ξεπεράσεις τις 168 ώρες της εβδομάδας!")
             
@@ -54,16 +58,20 @@ def task_add(tasks, total_hours):
 
 
 def task_edit():
+    # Αν δεν υπάρχουν Tasks τότε δεν μπορεί να γίνει και Edit.
     if not tasks:
         print("Δεν υπάρχουν στόχοι για τροποποίηση.")
         return
 
     print("Επέλεξε το όνομα του Task που θέλεις να τροποποιήσεις: ")
+    # Έλεγχος και εκτύπωση του Task List, i+1 γιατί είναι 0-Indexed
     for i, task in enumerate(tasks):
         print(f"{i + 1}. {task['name']}")
 
     try:
+        # -1 Για να γίνει σωστά η μετατροπή σε 0-Indexed
         task_index = int(input("Δώσε τον αριθμό του Task που θέλεις να κάνεις τροποποίηση: ")) - 1
+        # Έλεγχος αν είναι μέσα στα όρια.
         if task_index < 0 or task_index >= len(tasks):
             print("Άκυρη επιλογή, παρακαλώ δώστε τον αριθμό του Task.")
             return
@@ -71,6 +79,7 @@ def task_edit():
         print("Παρακαλώ εισάγετε έναν έγκυρο αριθμό.")
         return
 
+    #Επιλογή του Task τροποποίησης.
     current_task = tasks[task_index]
 
     new_name = input("Δώστε το νέο όνομα του Task: ")
@@ -91,7 +100,31 @@ def task_edit():
     else:
         print("Άκυρη τιμή σημαντικότητας του Task. Παρακαλώ προσπαθήστε ξανά.")
         
+def task_del():
+    if not tasks:
+        print("Δεν υπάρχουν στόχοι για διαγραφή.")
+        return
 
+    print("Επέλεξε το όνομα του Task που θέλεις να διαγράψεις: ")
+    # Έλεγχος και εκτύπωση του Task List, i+1 γιατί είναι 0-Indexed
+    for i, task in enumerate(tasks):
+        print(f"{i + 1}. {task['name']}")
+
+    try:
+        # -1 Για να γίνει σωστά η μετατροπή σε 0-Indexed
+        task_index = int(input("Δώσε τον αριθμό του Task που θέλεις να κάνεις διαγράψεις: ")) - 1
+        # Έλεγχος αν είναι μέσα στα όρια.
+        if task_index < 0 or task_index >= len(tasks):
+            print("Άκυρη επιλογή, παρακαλώ δώστε τον αριθμό του Task.")
+            return
+    except ValueError:
+        print("Παρακαλώ εισάγετε έναν έγκυρο αριθμό.")
+        return
+
+    #Επιλογή του Task και διαγραφή
+    tasks.pop(task_index)
+
+#Infinite Loop μέχρι την Manual έξοδο του χρήστη με τον αριθμό 4 ή Ctrl+C
 while True:
     display_menu()
     user_input = int(input("Επέλεξε έναν αριθμό απο το 1-4: "))
@@ -99,6 +132,7 @@ while True:
         total_hours = task_add(tasks, total_hours)
     if(user_input == 2):
         task_edit()
+    if(user_input == 3):
+        task_del()
     if(user_input == 4):
         break
-    
